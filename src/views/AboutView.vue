ec8285c7dc6a818768b4015045875cd4b29bdbd5
Chopinnn <353155692@qq.com>
2024-08-07 12:52:51 +0800

    feat: cicd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p @click="triggerTypeError()">触发TypeError</p>
+    <el-divider></el-divider>
+    <p @click="triggerReferenceError()">触发ReferenceError</p>
+    <el-divider></el-divider>
+    <p @click="triggerSyntaxError()">触发SyntaxError</p>
   </div>
 </template>
+
+<script setup lang="ts">
+const triggerTypeError = () => {
+  if (typeError.length > 0) {
+    console.log('err!')
+  }
+}
+const triggerReferenceError = () => {
+  throw new ReferenceError('Hello')
+}
+const triggerSyntaxError = () => {
+  throw new SyntaxError('Hello')
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
