35e397c7d9fc9262c466dd44a7d9b96655a86ed2
Chopinnn <353155692@qq.com>
2024-08-07 13:15:19 +0800

    feat: 测试cicd

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p @click="triggerTypeError()">触发TypeError</p>
+    <p @click="triggerTypeError()">hjz触发TypeError</p>
     <el-divider></el-divider>
-    <p @click="triggerReferenceError()">触发ReferenceError</p>
+    <p @click="triggerReferenceError()">hjz触发ReferenceError</p>
     <el-divider></el-divider>
-    <p @click="triggerSyntaxError()">触发SyntaxError</p>
+    <p @click="triggerSyntaxError()">hjz触发SyntaxError</p>
   </div>
 </template>
 
