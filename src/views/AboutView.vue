ee8c2ec698052d0ccf3af12275f6334dae7162e4
Chopinnn <353155692@qq.com>
2024-08-07 13:29:42 +0800

    feat: 修改workflows

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p @click="triggerTypeError()">hjz触发TypeError1</p>
+    <p @click="triggerTypeError()">hjzz触发TypeError1</p>
     <el-divider></el-divider>
-    <p @click="triggerReferenceError()">hjz触发ReferenceError1</p>
+    <p @click="triggerReferenceError()">hjzz触发ReferenceError1</p>
     <el-divider></el-divider>
-    <p @click="triggerSyntaxError()">hjz触发SyntaxError1</p>
+    <p @click="triggerSyntaxError()">hjzz触发SyntaxError1</p>
   </div>
 </template>
 
